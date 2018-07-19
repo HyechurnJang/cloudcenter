dcf2f6d1bfe993f595fcd8c7cc5d88056b8f0c97
jzide <jzide@10.139.4.34>
2018-07-19 13:02:52 +0900

    update

--- extctrl/engine/__init__.py
+++ extctrl/engine/__init__.py
@@ -10,57 +10,101 @@ from pygics import rest, Lock
 lock = Lock()
 controller = Controller('10.72.86.21', 'admin', '1234Qwer')
 
-def create_application(tn_name, bd_name, ap_name, epg_names):
+@rest('GET', '/apic/create/tn')
+def create_tn(request, tn_name):
     lock.on()
     try:
-        tenant = controller.Tenant.create(name=tn_name)
-        bd = tenant.BridgeDomain.create(name=bd_name)
-        ap = tenant.AppProfile.create(name=ap_name)
-        epgs = []
-        for epg_name in epg_names:
-            epg = ap.EPG.create(name=epg_name)
-            epg.relate(bd)
-            epgs.append(epg)
+        tn = controller.Tenant.create(name=tn_name)
     except Exception as e:
-        print str(e)
         ret = {'result' : None, 'error' : str(e)}
     else:
-        ret = {
-            'result' : {
-                'tn' : tenant,
-                'bd' : bd,
-                'ap' : ap,
-                'epgs' : epgs
-            }
-        }
+        ret = {'result' : tn}
     lock.off()
     return ret
 
-def delete_application(tn_name, bd_name, ap_name, epg_names):
+@rest('GET', '/apic/delete/tn')
+def delete_tn(tn_name):
     lock.on()
     try:
-        tenant = controller.Tenant(tn_name)
-        bd = tenant.BridgeDomain(bd_name)
-        ap = tenant.AppProfile(ap_name)
-        epgs = ap.EPG.list()
-        for epg in epgs: epg.delete()
-        ap.delete()
-        bd.delete()
-        tenant.delete()
+        controller.Tenant(tn_name).delete()
     except Exception as e:
-        print str(e)
         ret = {'result' : False, 'error' : str(e)}
     else:
         ret = {'result' : True}
     lock.off()
     return ret
 
-@rest('GET', '/apic/create')
-def create(request, tn_name, bd_name, ap_name, epg_names):
-    epg_names = epg_names.split(',')
-    return create_application(tn_name, bd_name, ap_name, epg_names)
+@rest('GET', '/apic/create/bd')
+def create_bd(tn_name, bd_name):
+    lock.on()
+    try:
+        tn = controller.Tenant(tn_name)
+        bd = tn.BridgeDomain.create(name=bd_name)
+    except Exception as e:
+        ret = {'result' : None, 'error' : str(e)}
+    else:
+        ret = {'result' : bd}
+    lock.off()
+    return ret
 
-@rest('GET', '/apic/delete')
-def delete(request, tn_name, bd_name, ap_name, epg_names):
-    epg_names = epg_names.split(',')
-    return delete_application(tn_name, bd_name, ap_name, epg_names)
+@rest('GET', '/apic/delete/bd')
+def delete_bd(tn_name, bd_name):
+    lock.on()
+    try:
+        controller.Tenant(tn_name).BridgeDomain(bd_name).delete()
+    except Exception as e:
+        ret = {'result' : False, 'error' : str(e)}
+    else:
+        ret = {'result' : True}
+    lock.off()
+    return ret
+
+@rest('GET', '/apic/create/ap')
+def create_ap(tn_name, ap_name):
+    lock.on()
+    try:
+        tn = controller.Tenant(tn_name)
+        ap = tn.AppProfile.create(name=ap_name)
+    except Exception as e:
+        ret = {'result' : None, 'error' : str(e)}
+    else:
+        ret = {'result' : ap}
+    lock.off()
+    return ret
+
+@rest('GET', '/apic/delete/ap')
+def delete_ap(tn_name, ap_name):
+    lock.on()
+    try:
+        controller.Tenant(tn_name).AppProfile(ap_name).delete()
+    except Exception as e:
+        ret = {'result' : False, 'error' : str(e)}
+    else:
+        ret = {'result' : True}
+    lock.off()
+    return ret
+
+@rest('GET', '/apic/create/epg')
+def create_epg(tn_name, ap_name, epg_name):
+    lock.on()
+    try:
+        ap = controller.Tenant(tn_name).AppProfile(ap_name)
+        epg = ap.EPG.create(epg_name)
+    except Exception as e:
+        ret = {'result' : None, 'error' : str(e)}
+    else:
+        ret = {'result' : epg}
+    lock.off()
+    return ret
+
+@rest('GET', '/apic/delete/epg')
+def delete_epg(tn_name, ap_name, epg_name):
+    lock.on()
+    try:
+        controller.Tenant(tn_name).AppProfile(ap_name).EPG(epg_name).delete()
+    except Exception as e:
+        ret = {'result' : False, 'error' : str(e)}
+    else:
+        ret = {'result' : True}
+    lock.off()
+    return ret
